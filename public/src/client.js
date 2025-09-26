733eb2ab6584ec037088d4000269b063ae19792d
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-09-26 16:07:23 -0400

    moved on submit event for search-topics-form from script element to searchTopics.js module

--- public/src/client.js
+++ public/src/client.js
@@ -6,4 +6,15 @@ require('./app');
 // from plugins that add files to "scripts" block in plugin.json
 require('../scripts-client');
 
+require(['searchTopics'], function (searchTopics) {
+	function initSearchTopics() {
+		$('[component="search/topics"]').each(function () {
+			searchTopics.init($(this));
+		});
+	}
+
+	$(window).on('action:ajaxify.end', initSearchTopics);
+	$(document).ready(initSearchTopics);
+});
+
 app.onDomReady();
---
+++ public/src/modules/searchTopics.js
@@ -0,0 +1,18 @@
+define('searchTopics', [], function () {
+	const searchTopics = {};
+
+	searchTopics.init = function (el) {
+		if (!el || !el.length) {
+			return;
+		}
+
+		el.on('submit', function (event) {
+			event.preventDefault();
+
+			const query = el.find('#search-topics').val().trim();
+			console.log('Search request received for: ', query);
+		});
+	};
+
+	return searchTopics;
+});
