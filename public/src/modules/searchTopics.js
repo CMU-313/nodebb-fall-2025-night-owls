5e3dfe50a8e9dc2bc4d62f29b4e625691fed043d
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-10-01 23:57:32 -0400

    Fuse integrated into searchTopics module (with example posts array)

--- public/src/modules/searchTopics.js
+++ public/src/modules/searchTopics.js
@@ -1,4 +1,4 @@
-define('searchTopics', [], function () {
+define('searchTopics', ['fuse.js'], function (Fuse) {
 	const searchTopics = {};
 
 	searchTopics.init = function (el) {
@@ -11,6 +11,20 @@ define('searchTopics', [], function () {
 
 			const query = el.find('#search-topics').val().trim();
 			console.log('Search request received for: ', query);
+
+			const allPostsExample = [
+				{pid: 1, content: 'This is a test post about JavaScript.'},
+				{pid: 2, content: 'Another post discussing Python programming.'},
+			];
+
+			const fuse = new Fuse(allPostsExample, {
+				includeScore: false,
+				keys: ['content'],
+			});
+
+			const results = fuse.search(query);
+
+			console.log('Search results:', results);
 		});
 	};
 
