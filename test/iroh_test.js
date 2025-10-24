aba5b0146c3c50e5def16bd2ba9c9b5375bb953a
Andrews Akwei <aakwei@andrew.cmu.edu>
2025-10-24 00:56:29 +0000

    iroh_test added and iroh installed

---
+++ test/iroh_test.js
@@ -0,0 +1,37 @@
+const Iroh = require('iroh');
+
+// Define your function as a string
+const code = `
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+// Call the function
+console.log('Result:', factorial(3));
+`;
+
+// Create an iroh stage and add listeners
+const stage = new Iroh.Stage(code);
+
+// Add event listeners to trace execution
+stage.addListener(Iroh.CALL).on('before', (e) => {
+	console.log('  '.repeat(e.indent) + `call ${e.name} ( [${e.arguments}] )`);
+});
+
+stage.addListener(Iroh.CALL).on('after', (e) => {
+	console.log('  '.repeat(e.indent) + `call ${e.name} end -> [${e.return}]`);
+});
+
+stage.addListener(Iroh.IF).on('enter', (e) => {
+	console.log('  '.repeat(e.indent) + 'if');
+});
+
+stage.addListener(Iroh.IF).on('leave', (e) => {
+	console.log('  '.repeat(e.indent) + 'if end');
+});
+
+// Execute the instrumented code
+eval(stage.script);
